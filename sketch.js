09724dc7e8fc04ad22e41b7da590be260e64479c
github username <evan@heyevan.com>
2022-05-07 15:52:31 -0700

    adds sound cloud

--- sketch.js
+++ sketch.js
@@ -9,35 +9,59 @@ let pointY3
 let col = 1
 let easing = 0.5
 
-
+let backgroundArr;
 function setup() {
-  createCanvas(windowWidth, windowHeight,WEBGL);
- 
+  frameRate(60)
+  createCanvas(windowWidth/1.25, windowHeight,WEBGL);
   capture = createCapture(VIDEO);
   capture.hide()
   circX=0
   circY=0
   numArray=[...Array(100).keys()]
-
+  pointX1=0
+  pointX1=0
+  ellipseMode(CENTER)
   }
 
 function draw() {
-
+  
   background('#cab7ff'); 
   
   r = random(255); // r is a random number between 0 - 255
-  g = random(100,200); // g is a random number betwen 100 - 200
-  b = random(100); // b is a random number between 0 - 100
-  a = random(0,50); // a is a random number between 200 - 255
-  width1 = windowWidth/220
-  height1 = windowHeight/220
+  g = random(255); // g is a random number betwen 100 - 200
+  b = random(255); // b is a random number between 0 - 100
+  a = random(0,75); // a is a random number between 200 - 255
+  width1 = 0
+  height1 = 0
   width2 = windowWidth/189
   height2 = windowWidth/189
-  pointX1 = -windowWidth
-  pointY1= -windowHeight
+  // pointX1 = -windowWidth
+  // pointY1= -windowHeight
   // if (second() % 2 === 0) {
-    backgroundTris()
+
     
+ 
+    // orbitControl(2, 3, 0.05) 
+    
+   
+    // backgroundDots()
+    if (second() % 3 === 0) {
+     backgroundDots()
+    } else {
+      backgroundTris()
+    }
+    texture(capture)
+  
+   
+    stroke(255)
+    strokeWeight(5) 
+    texture(capture)
+    ellipse(circX, circY, windowWidth/4, windowHeight/1.15) 
+
+    if (mouseIsPressed == true) {
+      circX = mouseX-windowWidth/2;
+      circY = mouseY-windowHeight/2;
+    }
   // } else if (second() % 5 === 0) {
   //   backgroundTris()
   // }
@@ -48,15 +72,7 @@ function draw() {
   // point(5, windowWidth/16)
   //CAM ELLIPSE
  //tri point order-bottom left,bottom right, top
-  // texture(capture)
  
-  // orbitControl(2, 3, 0.05) 
-  
-  if (mouseIsPressed == true) {
-    circX = mouseX-windowWidth/2;
-    circY = mouseY-windowHeight/2;
-  }
-  ellipse(circX, circY, windowWidth/4, windowHeight/1.15) 
 }
 
   
@@ -69,24 +85,65 @@ function windowResized(){
 
 const backgroundDots=()=>{
   //BACKGROUND dots
-  for (i=0; i < windowWidth; i+=10) {
-    for (j=0; j < windowHeight; j+=10) {
+
+  if (second() <= 5){
+    width1+=3
+    height1+=3
+    // r+=10
+    // g-=10
+    // b+=10
+    // a+=10
+  
+  } else if (second() > 5 && second() <=10) {
+    width1-=4
+    height1-=4
+    // r-=10
+    // g+=10
+    // b-=10
+    // a-=10
+    
+  } else if (second() > 10 && second() <=20){
+    width2+=10
+    height2+=10
+    // r+=10
+    // g-=10
+    // b+=10
+    // a-=10
+  } else if (second() > 20 && second()<=40){
+    width2-=2
+    height2-=2
+    // r-=10
+    // g+=10
+    // b-=10
+    // a+=10
+  } else if (second() > 40 && second() <=60) {
+    width2+=5
+    height2+=5
+    // r+=10
+    // g+=10
+    // b+=10
+  } else {
+  
+  }
+
+  for (i=0; i < windowWidth; i+=30) {
+    for (j=0; j < windowHeight; j+=30) {
       // at random intervals between 1 and 7 seconds, call either an ellipse or a triangle
       //60 frames per second 
-      noFill()
-      strokeWeight(1)
+      noStroke()
+     setInterval(function(){fill(r,g,b,a)}, 0.25)
       ellipse(
-        0 + i, 
-        0 + j, 
-        width1, 
-        height1
+        -windowWidth/2 + i, 
+        -500 + j, 
+        width2, 
+        height2
       )
     }
-    strokeWeight(5)
-    noFill()
+   noStroke()
+    setInterval(function(){fill(r,g,b,a)}, 0.5)
     ellipse(
-      windowWidth/2 + i * 36, 
-      windowHeight/2 + j * 36, 
+      0 + i , 
+      -windowHeight/2 + j, 
       width2, 
       height2
     )
@@ -95,48 +152,76 @@ const backgroundDots=()=>{
 
 
 const backgroundTris=()=>{
-strokeWeight(10)
+
 
 if (second() <= 5){
-  circX+=2
+  pointX1+=30
+  pointY1+=30
+  // r+=10
+  // g-=10
+  // b+=10
+  // a+=10
+
 } else if (second() > 5 && second() <=10) {
-  circX-=2
+  pointX1-=40
+  pointY1-=40
+  // r-=10
+  // g+=10
+  // b-=10
+  // a-=10
+  
 } else if (second() > 10 && second() <=20){
-  circX+=2
+  pointX1+=40
+  pointY1+=40
+  // r+=10
+  // g-=10
+  // b+=10
+  // a-=10
 } else if (second() > 20 && second()<=40){
-  circX-=2
+  pointX1+=40
+  pointY1+=40
+  // r-=10
+  // g+=10
+  // b-=10
+  // a+=10
+} else if (second() > 40 && second() <=60) {
+  pointX1-=50
+  pointY1-=50
+  // r+=10
+  // g+=10
+  // b+=10
 } else {
-  circX+=2
+
 }
 
-if(second() % 2 === 0 ) {
-fill(r,g,b,a)
-}
-triangle(circX, 36, 123, 225, 480, 36)
 
 
+// if(second() % 2 === 0 ) {
 
+// }
+// noStroke()
+fill(`rgba(249, 58, 37, 0.15)`)
+// triangle(circX, 36, 123, 225, 480, 36)
 
-
-  for (i=0; i<windowWidth; i+=150) {
-      for(j=0;j<windowWidth; j+=150) {
+  for (i=0; i<windowWidth; i+=250) {
+      for(j=0;j<windowWidth; j+=250) {
       // at random intervals between 1 and 7 seconds, call either an ellipse or a triangle
       //60 frames per second 
-      
+     
+      noStroke()
       triangle(
-        circX/2+j,
-        -windowHeight/2+i, 
+        pointX1/2+j,
+        -windowHeight/36+i, 
         -windowWidth+300+j, 
-        -windowHeight/1600+i, 
-        -windowWidth/45+j, 
-        -windowHeight/250+i
+        -windowHeight+i, 
+        -windowWidth+j, 
+        -windowHeight/+i
        )
     }
    
-    noFill()
     strokeWeight(1)
     triangle(
-    -windowWidth/2+i,
+    -pointY1/2+i,
     -windowHeight/36+j, 
     -windowWidth+i, 
     -windowHeight/16+j, 
@@ -144,7 +229,6 @@ triangle(circX, 36, 123, 225, 480, 36)
     -windowHeight/250+j
     )
   }
- pointX1+=30
-  pointY1+=30 
+ 
 }
 
